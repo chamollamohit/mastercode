396b28799a6bab4d36838a18d84ee8b11bd85a84
chamollamohit <mohitchamolla@gmail.com>
2026-02-27 00:53:13 +0530

    fix: added dynamic url

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: process.env.FRONT_END_URL,
         credentials: true,
     }),
 );
